b72fde58aadd28eae3031babd1b28959fdfd123f
Ravindu <theravindu@gmail.com>
2019-09-07 20:46:38 -0400

    Update attributesetfinder

--- AttributeSetFinder.py
+++ AttributeSetFinder.py
@@ -5,37 +5,34 @@ import random
 import re
 import sys
 
-def attributesSet(numberOfAttributes, supportThreshold):
-    import csv
-    import pandas as pd
-    import itertools
-    import math
+numberOfAttributes = int(4)
+supportThreshold = float(.6)
 
-    names = ['age','sex','education','country','race','status','workclass','occupation','hours-per-week','income','capital-gain','capital-loss']
-    combinations = []
-    final = []
-    for comb in itertools.combinations(names,numberOfAttributes):
-        combinations.append(list(comb))
-    url = "https://s3.amazonaws.com/istreet-questions-us-east-1/443605/census.csv"
-    c = pd.read_csv(url)
-    c.columns= names
-    total = len(c.index)
-    required = math.ceil(supportThreshold*total)
 
-    for i in combinations:
-        g = c.groupby(i).size().sort_values(ascending=False)
-        g
-        groups = g[g>required].index
-        satisfied = list(groups)
-        for j in satisfied:
-            final.append(','.join(j))
+import csv
+import pandas as pd
+import itertools
+import math
 
-    return final
+names = ['age','sex','education','country','race','status','workclass','occupation','hours-per-week','income','capital-gain','capital-loss']
+combinations = []
+final = []
+for comb in itertools.combinations(names,numberOfAttributes):
+    combinations.append(list(comb))
+url = "https://s3.amazonaws.com/istreet-questions-us-east-1/443605/census.csv"
+c = pd.read_csv(url)
+c.columns= names
+c.head()
+total = len(c.index)
+required = math.ceil(supportThreshold*total)
 
-if __name__ == '__main__':
+for i in combinations:
+    g = c.groupby(i).size().sort_values(ascending=False)
+    g
+    groups = g[g>required].index    
+    satisfied = list(groups)
+    for j in satisfied:
+        final.append(','.join(j))
 
-    numberOfAttributes = int(3)
-    supportThreshold = float(.7)
-    result = attributesSet(numberOfAttributes, supportThreshold)
-
-    print(result)
+for elem in final:
+    print(elem)
