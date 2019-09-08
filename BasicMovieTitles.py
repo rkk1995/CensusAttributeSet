c7c9cff6aac3d84efb8c6ab5d8220cee38d966dc
Ravindu <theravindu@gmail.com>
2019-09-07 21:09:26 -0400

    BasicMovieSearch

---
+++ BasicMovieTitles.py
@@ -0,0 +1,22 @@
+def  getMovieTitles(substr):
+    if not isinstance(substr,str):
+        return None
+    import json, requests
+    final = []
+    r = requests.get("https://jsonmock.hackerrank.com/api/movies/search/?Title=" + substr)
+    a = json.loads(r.text)
+    totalpages = a['total_pages']
+    if totalpages == 0:
+        return None
+    for i in range(1,totalpages+1):
+        r = requests.get("https://jsonmock.hackerrank.com/api/movies/search/?Title="+ substr + "&page=" + str(i))
+        a = json.loads(r.text)
+        for movie in a['data']:
+            final.append(movie['Title'])
+    return sorted(final)
+
+
+result = getMovieTitles('Spiderman')
+print(result)
+
+'hello'
